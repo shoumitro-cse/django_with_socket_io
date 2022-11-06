48da18e403301bac9b5da0ff6555a7a1c1b4e73c
shoumitro-cse <shoumitro26@gmail.com>
2022-11-07 01:25:39 +0600

    Class namespace client side fixed

--- socketio_app/views.py
+++ socketio_app/views.py
@@ -11,107 +11,107 @@ def index(request):
     return render(request, 'index.html')
 
 
-def background_thread():
-    """Example of how to send server generated events to clients."""
-    count = 0
-    while True:
-        sio.sleep(10)
-        count += 1
-        sio.emit('my_response', {'data': 'Server generated event'},
-                 namespace='/test')
-
-
-@sio.event
-def my_event(sid, event_data):
-    sio.emit('my_response', {'data': event_data['data']}, room=event_data['room_id'])
-
-
-@sio.event
-def my_broadcast_event(sid, message):
-    sio.emit('my_response', {'data': message['data']})
-
-
-@sio.event
-def join(sid, message):
-    sio.enter_room(sid, message['room'])
-    sio.emit('my_response', {'data': 'Entered room: ' + message['room']},
-             room=sid)
-
-
-@sio.event
-def leave(sid, message):
-    sio.leave_room(sid, message['room'])
-    sio.emit('my_response', {'data': 'Left room: ' + message['room']},
-             room=sid)
-
-
-@sio.event
-def close_room(sid, message):
-    sio.emit('my_response',
-             {'data': 'Room ' + message['room'] + ' is closing.'},
-             room=message['room'])
-    sio.close_room(message['room'])
-
-
-@sio.event
-def my_room_event(sid, message):
-    sio.emit('my_response', {'data': message['data']}, room=message['room'])
-
-
-@sio.event
-def disconnect_request(sid):
-    sio.disconnect(sid)
-    print("\n\ndisconnect sid: ", sid)
-    print()
-
-
-@sio.event
-def connect(sid, environ):
-    print("\nconnect sid: ", sid)
-    # print(environ)
-    # username = authenticate_user(environ)
-    username = "shoumitro26"
-    sio.save_session(sid, {'username': username})
-
-
-@sio.event
-def disconnect(sid):
-    print('Client disconnected')
-    print("\ndisconnect sid: ", sid)
-    print()
-
-
-@sio.event
-def begin_chat(sid, room_id):
-    sio.enter_room(sid, room_id)
-    print()
-    sio.emit('my_response', {'data': 'begin_chat', 'room_id': room_id}, room=room_id)
-    print("\nbegin_chat sid, room_id: ", sid, room_id)
-    print()
-
-
-@sio.event
-def exit_chat(sid, room_id):
-    sio.leave_room(sid, room_id)
-    print("\nexit_chat sid, room_id: ", sid, room_id)
-    print()
-
-
-@sio.event
-def message(sid, data):
-    session = sio.get_session(sid)
-    print('message from ', session['username'])
-
-
-@sio.event(namespace='/chat')
-def sms_event(sid, data):
-    pass
-
-
-@sio.on('my custom event', namespace='/chat')
-def my_custom_event(sid, event_data):
-    print("\n my_custom_event sid: ", sid)
-    sio.emit('my_response', {'data': event_data['data']}, room=event_data['room_id'])
+# def background_thread():
+#     """Example of how to send server generated events to clients."""
+#     count = 0
+#     while True:
+#         sio.sleep(10)
+#         count += 1
+#         sio.emit('my_response', {'data': 'Server generated event'},
+#                  namespace='/test')
+#
+#
+# @sio.event
+# def my_event(sid, event_data):
+#     sio.emit('my_response', {'data': event_data['data']}, room=event_data['room_id'])
+#
+#
+# @sio.event
+# def my_broadcast_event(sid, message):
+#     sio.emit('my_response', {'data': message['data']})
+#
+#
+# @sio.event
+# def join(sid, message):
+#     sio.enter_room(sid, message['room'])
+#     sio.emit('my_response', {'data': 'Entered room: ' + message['room']},
+#              room=sid)
+#
+#
+# @sio.event
+# def leave(sid, message):
+#     sio.leave_room(sid, message['room'])
+#     sio.emit('my_response', {'data': 'Left room: ' + message['room']},
+#              room=sid)
+#
+#
+# @sio.event
+# def close_room(sid, message):
+#     sio.emit('my_response',
+#              {'data': 'Room ' + message['room'] + ' is closing.'},
+#              room=message['room'])
+#     sio.close_room(message['room'])
+#
+#
+# @sio.event
+# def my_room_event(sid, message):
+#     sio.emit('my_response', {'data': message['data']}, room=message['room'])
+#
+#
+# @sio.event
+# def disconnect_request(sid):
+#     sio.disconnect(sid)
+#     print("\n\ndisconnect sid: ", sid)
+#     print()
+#
+#
+# @sio.event
+# def connect(sid, environ):
+#     print("\nconnect sid: ", sid)
+#     # print(environ)
+#     # username = authenticate_user(environ)
+#     username = "shoumitro26"
+#     sio.save_session(sid, {'username': username})
+#
+#
+# @sio.event
+# def disconnect(sid):
+#     print('Client disconnected')
+#     print("\ndisconnect sid: ", sid)
+#     print()
+#
+#
+# @sio.event
+# def begin_chat(sid, room_id):
+#     sio.enter_room(sid, room_id)
+#     print()
+#     sio.emit('my_response', {'data': 'begin_chat', 'room_id': room_id}, room=room_id)
+#     print("\nbegin_chat sid, room_id: ", sid, room_id)
+#     print()
+#
+#
+# @sio.event
+# def exit_chat(sid, room_id):
+#     sio.leave_room(sid, room_id)
+#     print("\nexit_chat sid, room_id: ", sid, room_id)
+#     print()
+#
+#
+# @sio.event
+# def message(sid, data):
+#     session = sio.get_session(sid)
+#     print('message from ', session['username'])
+#
+#
+# @sio.event(namespace='/chat')
+# def sms_event(sid, data):
+#     pass
+#
+#
+# @sio.on('my custom event', namespace='/chat')
+# def my_custom_event(sid, event_data):
+#     print("\n my_custom_event sid: ", sid)
+#     sio.emit('my_response', {'data': event_data['data']}, room=event_data['room_id'])
 
 
 class MyCustomNamespace(socketio.Namespace):
@@ -125,6 +125,7 @@ class MyCustomNamespace(socketio.Namespace):
 
     def on_begin_chat(self, sid, room_id):
         self.enter_room(sid, room_id)
+        print("room_id: ", room_id)
 
     def on_exit_chat(self, sid, room_id):
         self.leave_room(sid, room_id)
--- sync_client2.py
+++ sync_client2.py
@@ -1,4 +1,4 @@
-#It just needs to install more packages here.
+# It just needs to install more packages here.
 # pip install gevent-websocket
 # pip install eventlet
 
@@ -8,35 +8,60 @@
 import socketio
 
 sio = socketio.Client()
+room_id = 1234567
 
-@sio.event
-def connect():
-    print('connection established')
-    sio.emit('begin_chat', 1234567)
 
-@sio.event
-def my_message(data):
-    print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
+# @sio.event
+# def connect():
+#     print('connection established')
+#     sio.emit('begin_chat', room_id)
+#
+#
+# @sio.event
+# def my_message(data):
+#     print('message received with ', data)
+#     sio.emit('my response', {'response': 'my response'})
+#
+#
+# @sio.event
+# def connect_error(data):
+#     print("The connection failed!")
+#
+#
+# @sio.event
+# def disconnect():
+#     print('disconnected from server')
+#
+#
+# @sio.event
+# def my_response(data):
+#     print("\n\nmy_response data : ", data)
 
-@sio.event
-def connect_error(data):
-    print("The connection failed!")
-    
-@sio.event
-def disconnect():
-    print('disconnected from server')
 
-@sio.event
-def my_response(data):
-    print("\n\nmy_response data : ", data)
-    
-    
-sio.connect('http://localhost:8000')
-sio.wait
-#sio.emit('my_response', {'foo': 'bar'})
-#sio.emit('begin_chat', 1234567)
-#sio.emit('exit_chat', 1234567)
+# sio.connect('http://localhost:8000')
+# sio.connect('http://localhost:8000', namespaces=['/test'])
+# sio.wait
+
+
+# sio.emit('my_response', {'foo': 'bar'})
+# sio.emit('begin_chat', 1234567)
+# sio.emit('exit_chat', 1234567)
 # sio.send("response")
 
 
+class MyCustomNamespace(socketio.ClientNamespace):
+    def on_connect(self):
+        self.emit('begin_chat', room_id)
+        print('connection established')
+
+    def on_disconnect(self):
+        print('disconnected')
+
+    def on_my_response(self, data):
+        print("\nmy_response data : ", data)
+
+
+sio.register_namespace(MyCustomNamespace('/test'))
+
+sio.connect('http://localhost:8000', namespaces=['/test'])
+sio.wait
